3cdd9970f6031f4fa292a15107007a283dec47c6
André Gomes <andre-gomes@live.fr>
2018-06-19 16:24:03 +0200

    remove unused props

--- src/components/HierarchyNavigation/HierarchyItems.js
+++ src/components/HierarchyNavigation/HierarchyItems.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Item from "./Item";
 
-export default ({ childs, show, handleItemsToShow }) =>
-  childs.map(({ navigationNode: { navigationItem, childs }, isFirstLevel }) => (
-    <Item
-      navigationItem={navigationItem}
-      childs={childs}
-      isFirstLevel={isFirstLevel}
-    />
-  ));
+export default class HierarchyItems extends Component {
+  render() {
+    const { childs } = this.props;
+    return childs.map(
+      ({ navigationNode: { navigationItem, childs }, isFirstLevel }) => (
+        <Item
+          navigationItem={navigationItem}
+          childs={childs}
+          isFirstLevel={isFirstLevel}
+        />
+      )
+    );
+  }
+}
