7e941763e0e142f6defab5bafb984b2d61dbeb25
André Gomes <andre-gomes@live.fr>
2018-06-20 17:49:15 +0200

    remove css

--- src/index.js
+++ src/index.js
@@ -2,15 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HierarchyNavigation from "./components/HierarchyNavigation";
 
-// import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <HierarchyNavigation />
-    </div>
-  );
-}
-
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<HierarchyNavigation />, rootElement);
